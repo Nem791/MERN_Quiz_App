1da59d035f64b2bbaa0ed027ed25676546d7a6bc
Nem791 <phuongnam131201.nnnpn@gmail.com>
2022-06-01 20:48:06 +0700

    update

--- back_end/app.js
+++ back_end/app.js
@@ -1,48 +1,53 @@
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-const fileUpload = require('express-fileupload');
-const MongoStore = require('connect-mongo');
+var express = require("express");
+var session = require("express-session");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const MongoStore = require("connect-mongo");
+const cors = require("cors");
 
-const db = 'mongodb://localhost:27017/quiz_db';
+const db = "mongodb://localhost:27017/quiz_db";
 const connection = mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users'); 
-var quizzesRouter = require('./routes/quiz');
-var submitRouter = require('./routes/submit'); 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var quizzesRouter = require("./routes/quiz");
+var submitRouter = require("./routes/submit");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set up express-session dung connect-mongo
-app.use(session({
+app.use(
+  session({
     resave: false,
     saveUninitialized: true,
     secret: "keyboard cat",
     cookie: { maxAge: 8 * 60 * 60 * 1000 },
     store: MongoStore.create({
-        mongoUrl: db
-    })
-}));
+      mongoUrl: db,
+    }),
+  })
+);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/quizzes', quizzesRouter);
-app.use('/submit', submitRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/quizzes", quizzesRouter);
+app.use("/submit", submitRouter);
 
 module.exports = app;
