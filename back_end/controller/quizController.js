0d7a181f57f0f699282d6a88116815b28dc2a3a1
Nem791 <phuongnam131201.nnnpn@gmail.com>
2022-06-04 16:29:10 +0700

    update

--- back_end/controller/quizController.js
+++ back_end/controller/quizController.js
@@ -251,6 +251,45 @@ const saveQuiz = async (req, res) => {
     // const quizList = await Quiz.create()
 };
 
+const savePost = async (req, res) => {
+    // Luu DataTransferItemList, content, image to database 
+    console.log('req.body-', req.body);
+    console.log('req.user-', req.user);
+    let image = req.files.image;
+    let files = req.files;
+    for (const key in files) {
+        console.log('file');
+        console.log(files[key]);
+        req.body = { ...req.body, [key]: '/upload/' + files[key].name }
+        // files[key].mv(path.join(__dirname, '..', '/public/upload/', image.name), function (error) {
+        //     console.log("error: ", error);
+        // });
+        await fileUploadAsync(files[key]);
+
+    }
+
+    // if (Array.isArray(files)) {
+    //     try {
+    //         const data = await Promise.all(files.map((x) => fileUploadAsync(x)));
+    //         console.log("data: ", data);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // } else if (typeof files === 'object') {
+    //     return fileUploadAsync(files);
+    // }
+
+    console.log(req.body);
+
+    await BlogPost.create({
+        ...req.body,
+        user: mongoose.Types.ObjectId(req.user._id)
+    }, function (err) {
+        console.log(err);
+    })
+
+};
+
 const recommendArticle = (req, res) => {
     const posts = BlogPost.aggregate();
 }
